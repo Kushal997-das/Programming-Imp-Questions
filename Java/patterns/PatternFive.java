dc6fdf6821c918c17655174d12aabfbe4f9aeb87
Tapaswini kar <kar.tapaswini1111@gmail.com>
2020-10-04 13:41:45 +0530

    some pattern problems are added

--- Java/patterns/PatternFive.java
+++ Java/patterns/PatternFive.java
@@ -1,4 +1,20 @@
 package easy.patterns;
 
 public class PatternFive {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        for( int i = 0; i<n ; i++){
+            int count = 1;
+            for(int j = 0; j<=i ;j++){
+                System.out.print(count+"	");
+                int val = count * (i - j)/ (j + 1);
+                count =val;
+            }
+            System.out.println();
+           
+        }
+
+    }
 }
--- Java/patterns/PatternThree.java
+++ Java/patterns/PatternThree.java
@@ -1,4 +1,25 @@
 package easy.patterns;
 
 public class PatternThree {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int sp =n-1;
+        int st=1;
+        for(int i = 0;i<n;i++){
+            for(int j =0;j<sp;j++){
+                System.out.print("\t");
+            }
+            
+            for(int j=0;j<st;j++){
+                
+                System.out.print("*\t");
+                
+            }
+            sp--;
+            st++;
+            System.out.println();
+        }
+
+    }
 }
